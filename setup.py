5f0ba9da75930c8b41903fb6965555d8d2f45977
Andreas Jaeger <aj@suse.com>
2020-03-04 20:46:18 +0100

    Update python files for OpenDev
    Update Python files for move to OpenDev.
    
    Update openstackdocstheme also to newer version.
    
    Change-Id: I2184e381f587c2630e514e44fa9a9f3bbc4d65e2

--- setup.py
+++ setup.py
@@ -14,16 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import setuptools
-
-# In python < 2.7.4, a lazy loading of package `pbr` will break
-# setuptools if some other modules registered functions in `atexit`.
-# solution from: http://bugs.python.org/issue15881#msg170215
-try:
-    import multiprocessing  # noqa
-except ImportError:
-    pass
-
 setuptools.setup(
-    setup_requires=['pbr>=1.8'],
+    setup_requires=['pbr>=2.0.0'],
     pbr=True)
